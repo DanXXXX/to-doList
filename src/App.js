cbf10c374b62ce695cdfcdfe9ceffa272d6016e7
Victoire Nyoung <nyoungvictoire@gmail.com>
2021-08-26 13:38:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/main.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <>
+        <h1>  hello</h1>
+      </>
+    );
+  }
+}
+ 
+export default App;
